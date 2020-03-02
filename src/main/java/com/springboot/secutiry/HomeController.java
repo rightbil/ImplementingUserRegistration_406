b3009b26365e090d3e5fc271c52810f2ebcbe0d5
GBTC440017UR <GBTC440017UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-02 16:06:19 -0500

    to the end of 4.6

--- src/main/java/com/springboot/secutiry/HomeController.java
+++ src/main/java/com/springboot/secutiry/HomeController.java
@@ -1,28 +1,71 @@
 package com.springboot.secutiry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
 
 @Controller
 public class HomeController {
-@RequestMapping("/")
-    public String index(){
 
-    return "index";
-}
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model) {
+
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
+        model.addAttribute("user", user);
+        if (result.hasErrors()) {
+            return "registration";
+        } else {
+            userService.saveUser(user);
+            model.addAttribute("message", "User Account Created");
+
+        }
+        return "index";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+
+        return "index";
+    }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
+
     @RequestMapping("/admin")
-    public String admin(){
+    public String admin() {
         return "admin";
     }
 
-    @RequestMapping("/secure")
-    public String secure(){
-        return "secure";
-    }
+
+
 
 }
